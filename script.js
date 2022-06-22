90fa894b1dde3363e0bfa810ad1ee82900e341f1
edo979 <prezentabilan@gmail.com>
2022-06-22 12:52:02 +0200

    add js, start lightbox slider

---
+++ script.js
@@ -0,0 +1,11 @@
+const nextBtn = document.getElementById('lightbox-next'),
+  prevBtn = document.getElementById('lightbox-prev')
+
+// Event listeners
+nextBtn.addEventListener('click', (e) => {
+  console.log('next')
+})
+
+prevBtn.addEventListener('click', (e) => {
+  console.log('prev')
+})
